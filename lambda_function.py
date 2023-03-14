0a902378fd4a56c057eb4708836c8daf8942bda0
Eriyeza Buwembo <eriyeza.buwembo@mail.utoronto.ca>
2023-03-14 15:49:32 -0600

    working mvp for librarian program

--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,30 @@
 from librarian import send_to_kindle, download_epub
+import urllib.parse
 
 def lambda_handler(event, context):
     print("Received event: " + str(event))
-    url = event["Body"]
-    filename = download_epub(url)
-    if not filename:
+    if "Body" not in event:
+        print("Error - unexpected event: " + str(event))
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"\
+           "<Response><Message><Body>Unexpected event. -Lambda</Body></Message></Response>"
+    url = urllib.parse.unquote(event["Body"])
+    try:
+        filename, file_data = download_epub(url)
+    except:
+        print("Error - failed to download epub: " + str(event))
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"\
+           "<Response><Message><Body>Error downloading epub. -Lambda</Body></Message></Response>"
+    if not filename or not file_data:
+        print("Error - failed to download epub: " + str(event))
         return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"\
            "<Response><Message><Body>Error downloading epub. -Lambda</Body></Message></Response>"
     else:
         try:
-            send_to_kindle(filename)
+            send_to_kindle(filename, file_data)
+            print("Success!")
             return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"\
                 "<Response><Message><Body>Successful sent to kindle. -Lambda</Body></Message></Response>"
         except:
+            print("Error - failed to email epub: " + str(event))
             return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"\
             "<Response><Message><Body>Error sending epub to kindle. -Lambda</Body></Message></Response>"
--- librarian.py
+++ librarian.py
@@ -4,6 +4,7 @@ import sys
 import os
 import requests
 import smtplib
+#import boto3
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
@@ -12,6 +13,9 @@ from email.mime.text import MIMEText
 from_email = os.environ.get("EMAIL_ADDRESS")
 from_password = os.environ.get("EMAIL_PASSWORD")
 to_kindle_email = os.environ.get("KINDLE_EMAIL")
+#s3 = boto3.client('s3')
+
+#bucket_name = "erizza-bucket-librarian-1"
 
 """
 Given a text input, search on libgen for epub files and return a list of
@@ -21,6 +25,9 @@ def find_epub(title: str):
     results = []
     return results
 
+"""
+Download the epub.
+"""
 def download_epub(url):
     try:
         response = requests.get(url)
@@ -28,16 +35,15 @@ def download_epub(url):
         return ''
     filename = response.headers.get('Content-Disposition').split('"')[1]
     if response.status_code == 200:
-        with open(f"downloads/{filename}.epub", "wb") as f:
-            f.write(response.content)
-            print(f"{filename} saved successfully!")
-        return filename
+        #s3.put_object(Bucket=bucket_name, Key=filename, Body=response.content)
+        print(f"{filename} found!")
+        return filename, response.content
     else:
         print("Error downloading file")
-        return ''
+        return '', None
 
 
-def send_to_kindle(filename):
+def send_to_kindle(filename, file_data):
     # Set up the email message
     msg = MIMEMultipart()
     msg['From'] = from_email
@@ -45,11 +51,10 @@ def send_to_kindle(filename):
     msg['Subject'] = ''
 
     # Attach the epub file
-    with open(f"downloads/{filename}", 'rb') as f:
-        epub_attachment = MIMEApplication(f.read(), _subtype='epub')
-        epub_attachment.add_header(
-            'Content-Disposition', 'attachment', filename=f"{filename}")
-        msg.attach(epub_attachment)
+    epub_attachment = MIMEApplication(file_data, _subtype='epub')
+    epub_attachment.add_header(
+        'Content-Disposition', 'attachment', filename=f"{filename}")
+    msg.attach(epub_attachment)
 
     # Connect to the SMTP server and send the email
     with smtplib.SMTP('smtp.gmail.com', 587) as server:
@@ -60,6 +65,7 @@ def send_to_kindle(filename):
 
 if __name__ == "__main__":
     # print(find_epub("80 20 principle"))
-    link = "http://62.182.86.140/main/842000/8ece5c5ea1259bc706a12c2f1ec71206/Richard%20Koch%20-%20The%2080_20%20Principle_%20The%20Secret%20to%20Achieving%20More%20with%20Less-Crown%20Business%20%281999%29.epub"
-    title = download_epub(link)
-    send_to_kindle(title)
+    # link = "http://62.182.86.140/main/842000/8ece5c5ea1259bc706a12c2f1ec71206/Richard%20Koch%20-%20The%2080_20%20Principle_%20The%20Secret%20to%20Achieving%20More%20with%20Less-Crown%20Business%20%281999%29.epub"
+    link2 = "http://62.182.86.140/main/461000/e5fa82c4419247652fc53c543cbb823f/Miguel%20de%20Cervantes%20Saavedra%2C%20Edith%20Grossman%20%28Translator%29%2C%20Harold%20Bloom%20%28Introduction%29%20-%20Don%20Quixote-Harper%20Perennial%20%282005%29.epub"
+    title, data = download_epub(link2)
+    send_to_kindle(title, data)
